14599723ed4a9e51de7671456769a48717792260
Miklaan <milance_kucevo@hotmail.com>
2020-12-07 17:31:35 +0100

    Commit #2 - Miklaan

---
+++ index.js
@@ -0,0 +1,18 @@
+// Add padding-top to show content behind navbar
+
+// $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+
+// detect scroll up or download
+
+if($(".scroll-bar").length > 0) { // check if element exists
+  var last_scroll_top = 0;
+  $(window).on("scroll", function() {
+    scroll_top = $(this).scrollTop();
+    if(scroll_top < last_scroll_top) {
+      $(".scroll-bar").removeClass("scrolled-down").addClass("scrolled-up");
+    } else {
+      $(".scroll-bar").removeClass("scrolled-up").addClass("scrolled-down");
+    }
+    last_scroll_top = scroll_top;
+  });
+}
